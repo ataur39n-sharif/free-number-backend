3ea7566b024c20f11bffc9f4503774d444fb56a9
ataur39n-sharif <rahman.sharif39n@gmail.com>
2022-12-07 21:26:05 +0600

    app ready to handle onlinesim provider

--- Controllers/ReceiveSms/ReceiveSms.controller.js
+++ Controllers/ReceiveSms/ReceiveSms.controller.js
@@ -1,4 +1,6 @@
+const NumberModel = require("../../Models/Number/Number.model")
 const ReceiveSmsModel = require("../../Models/ReceiveSms/ReceiveSms.model")
+const OnlineSimUtils = require("../../utils/OnlineSim")
 
 const ReceiveSmsController = {
     new_sms: async (req, res) => {
@@ -34,7 +36,13 @@ const ReceiveSmsController = {
     allSms: async (req, res) => {
         try {
             const { number } = req.params;
-            const msgList = await ReceiveSmsModel.find({ receiver: number }).sort({createdAt:-1})
+            const numberInfo = await NumberModel.findOne({ phone_number: number })
+            let msgList = []
+            if (numberInfo.provider === 'onlineSim') {
+                msgList = await OnlineSimUtils.getAllMessages(`+${number}`, numberInfo?.country_code)
+            } else {
+                msgList = await ReceiveSmsModel.find({ receiver: number }).sort({ createdAt: -1 })
+            }
             return res.status(200).json({
                 success: true,
                 msgList
--- utils/OnlineSim/index.js
+++ utils/OnlineSim/index.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const NumberModel = require("../../Models/Number/Number.model");
+const CountryCodes = require("../CountryCodes");
 const countryList = require("../countryList");
 
 const OnlineSimUtils = {
@@ -30,6 +31,40 @@ const OnlineSimUtils = {
         return {
             update: true
         }
+    },
+    //get all messages for a number
+    getAllMessages: async (number, country_code) => {
+        // const list = await 
+        const calling_code = CountryCodes.find(each => each.code.toLowerCase() === country_code?.toLowerCase())
+        // console.log(calling_code);
+        const dial_digit = calling_code.dial_code.split('+')[1]
+        const number_withOut_dialDigit = number.split(calling_code.dial_code)[1]
+        // console.log(dial_digit, number_withOut_dialDigit);
+
+        const { data: { messages } } = await axios.get(`https://onlinesim.io/api/getFreeMessageList?phone=${number_withOut_dialDigit}&country=${dial_digit}&lang=en`)
+
+        const { data, last_page } = messages
+        let msgList = [...data]
+
+        if (last_page > 10) {
+            for (let i = 2; i <= 5; i++) {
+                const newPageData = await axios.get(`https://onlinesim.io/api/getFreeMessageList?phone=${number_withOut_dialDigit}&country=${dial_digit}&lang=en&page=${i}`)
+                msgList = [...msgList, ...newPageData?.data?.messages?.data]
+            }
+        }
+
+        let allMessages = []
+        for (let i = 0; i < msgList.length; i++) {
+            const { text, in_number, my_number, created_at } = msgList[i];
+            const modifyObj = {
+                receiver: number,
+                sender: in_number,
+                message: text,
+                createdAt: created_at
+            }
+            allMessages.push(modifyObj)
+        }
+        return allMessages
     }
 }
 
